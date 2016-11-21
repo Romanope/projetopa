e2cc6a5c23f4500ddec45dd23ae158e03a280839
Gabriel Gouveia <joao.gabriel.gouveia2@gmail.com>
2016-11-21 01:39:17 -0300

    tela para adicionar contato

---
+++ AppProjeto/app/src/main/java/com/progavancada/appprojeto/CadastroContato.java
@@ -0,0 +1,13 @@
+package com.progavancada.appprojeto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CadastroContato extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cadastro_contato);
+    }
+}
