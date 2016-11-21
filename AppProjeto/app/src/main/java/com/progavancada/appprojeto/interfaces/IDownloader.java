b8610b90fd38dc33cb7a065dee00b775f7d90772
lucasdinizv1 <lucasdinizvic@gmail.com>
2016-11-20 21:23:30 -0300

    criei uma interface pra usar os metodos enquanto não temos a LIB, aí quando terminar a lib a gente faz os metodos se conectarem com a lib

--- AppProjeto/app/src/main/java/com/progavancada/appprojeto/interfaces/IDownloader.java
+++ AppProjeto/app/src/main/java/com/progavancada/appprojeto/interfaces/IDownloader.java
@@ -1,8 +1,15 @@
 package com.progavancada.appprojeto.interfaces;
 
+import com.progavancada.appprojeto.model.Musica;
+import com.progavancada.appprojeto.model.Video;
+
 /**
  * Created by Lucas on 20/11/2016.
  */
 
 public interface IDownloader {
+
+    public Musica baixarMusica ();
+    public Video baixarVideo ();
+
 }
