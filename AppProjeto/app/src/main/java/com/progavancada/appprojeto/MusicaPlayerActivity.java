cd95b9e7829d70774430b8d7d8b62a237ccff65c
Gabriel Gouveia <joao.gabriel.gouveia2@gmail.com>
2016-12-03 00:21:58 -0300

    player musica funcional

--- AppProjeto/app/src/main/java/com/progavancada/appprojeto/MusicaPlayerActivity.java
+++ AppProjeto/app/src/main/java/com/progavancada/appprojeto/MusicaPlayerActivity.java
@@ -1,28 +1,113 @@
 package com.progavancada.appprojeto;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
+import android.os.Handler;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.progavancada.appprojeto.model.Musica;
+
+import org.w3c.dom.Text;
+
+import java.io.File;
 
 public class MusicaPlayerActivity extends AppCompatActivity {
 
     private Button mBtnPlay;
     private Button mBtnPause;
     private Button mBtnStop;
+    private TextView mTxtNomeMusica;
+    private TextView mTxtNomeAutor;
+    private Musica mMusica;
+    private ProgressBar mProgressoMusica;
+
+    private Handler mHandler;
+
+    private MediaPlayer mMediaPlayer;
+    private Uri mUriMusica;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_musica_player);
 
+        mHandler = new Handler();
+
+        Intent intent = getIntent();
+        mMusica = (Musica) intent.getSerializableExtra("musica");
+
         mBtnPlay = (Button) findViewById(R.id.btn_play);
         mBtnPause = (Button) findViewById(R.id.btn_pause);
         mBtnStop = (Button) findViewById(R.id.btn_stop);
+        mTxtNomeMusica = (TextView) findViewById(R.id.txt_nome_musica_player);
+        mTxtNomeAutor = (TextView) findViewById(R.id.txt_nome_autor_player);
 
-        MediaPlayer mediaPlayer = MediaPlayer.create(this, Uri.parse("http://www.myinstants.com/media/sounds/tindeck_1.mp3"));
-        mediaPlayer.start();
+        mTxtNomeMusica.setText(mMusica.getNome());
+        mTxtNomeAutor.setText(mMusica.getAutor());
+
+        mUriMusica = Uri.fromFile(new File(mMusica.getUrlMusica()));
+        mMediaPlayer = MediaPlayer.create(MusicaPlayerActivity.this, mUriMusica);
+
+        mProgressoMusica = (ProgressBar) findViewById(R.id.progressoMusica);
+        mProgressoMusica.setMax(mMediaPlayer.getDuration());
+
+        mBtnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    mMediaPlayer.start();
+                } catch (NullPointerException e) {
+                    Toast.makeText(MusicaPlayerActivity.this, "O arquivo não existe ou está vazio", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+
+        mBtnPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    mMediaPlayer.pause();
+                } catch (NullPointerException e) {
+                    Toast.makeText(MusicaPlayerActivity.this, "O arquivo não existe ou está vazio", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+
+        mBtnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    mMediaPlayer.stop();
+                    mMediaPlayer = MediaPlayer.create(MusicaPlayerActivity.this, mUriMusica);
+                } catch (NullPointerException e) {
+                    Toast.makeText(MusicaPlayerActivity.this, "O arquivo não existe ou está vazio", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        this.runOnUiThread(new Runnable() {
+
+            @Override
+            public void run() {
+                if(mMediaPlayer != null){
+                    int mCurrentPosition = mMediaPlayer.getCurrentPosition();
+                    mProgressoMusica.setProgress(mCurrentPosition);
+                }
+                mHandler.postDelayed(this, 200);
+            }
+        });
 
     }
 }
